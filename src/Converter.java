67543899c53891804368a98ab2315f930ad56474
NaDaChi09 <87379025+NaDaChi09@users.noreply.github.com>
2021-07-16 23:15:01 -0500

    runnable jar file added

---
+++ src/Converter.java
@@ -0,0 +1,70 @@
+
+import java.util.Scanner;
+
+class Converter {
+	static int i;
+	static int j;
+	static double result;
+	static Scanner s = new Scanner(System.in);
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+
+		// Variables
+		int menuSelection = 1;
+		int Quit = 0;
+
+		// While loop
+		while (menuSelection != Quit) {
+			System.out.println("Please select an option:\n" + "1. Teaspoons to Cups\n" + "2. Miles to Kilometers\n"
+					+ "3. US Gallons to Imperial Gallons\n" + "4. Quit");
+
+			// User Input for option
+			//Scanner s = new Scanner(System.in);
+			int Selection = s.nextInt(); // nextInt();
+			// switch operation
+			switch (Selection) {
+			case 1:
+				Tsp2Cups();
+				break;
+			case 2:
+				Mi2Km();
+				break;
+			case 3:
+				USgaltoIgal();
+				break;
+			case 4:
+				menuSelection = 0;
+				System.out.println("GoodBye!");
+				break;
+			default:
+				System.err.println("Try again...Please enter a number between 1 and 4.");
+			}
+		System.out.println("------------------------------------------------\n");
+		}
+		
+	}
+	
+	// static methods is used here so there is no need to create an instance
+	public static void Tsp2Cups() {
+		System.out.println("How many teaspoons?");
+		i = s.nextInt(); // nextInt();
+		j = i * 48;
+		System.out.println("There are " + j + " teaspoons in " + i + " cups.");
+	}
+
+	public static void Mi2Km() {
+		double Mi2Km = 0.621371;
+		System.out.println("How many miles?");
+		i = s.nextInt(); // nextInt();
+		result = (double) i * Mi2Km;
+		System.out.println("There are " + result + " kilometers in " + i + " miles.");
+	}
+
+	public static void USgaltoIgal() {
+		System.out.println("How many US Gals?");
+		i = s.nextInt(); // nextInt();
+		result = (double) i * 0.832674;
+		System.out.println("There are " + result + "US gals in " + i + " Imperial gals.");
+	}
+}
